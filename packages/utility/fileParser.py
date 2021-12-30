22f22406f55a740231d0051abdb2597c654aa3d6
Navid <navid.naseri.079@gmail.com>
2021-12-30 19:58:57 +0330

    added gantt file parser

--- packages/utility/fileParser.py
+++ packages/utility/fileParser.py
@@ -1,4 +1,6 @@
 from ..models.Process import Process
+from ..models.Burst import Burst
+from ..models.Gantt import Gantt
 
 def parseFile(fileName):
   with open(fileName, 'r') as file:
@@ -13,6 +15,25 @@ def parseFile(fileName):
     processes = list(map(lambda data: Process(data[0], data[1], data[2]), zip(labels, startTimes, burstTimes)))
     return processes, q1, q2
 
+def parseGanttFile(fileName):
+  with open(fileName, 'r') as file:
+    time = int(file.readline())
+    burstStrings = file.readlines()
+    burstStrings = [parseList(string) for string in burstStrings]
+    bursts = []
+    for string in burstStrings:
+      label, startTime, burstTime, runTime, endTime, burstStartTime, burstLength = string
+      process = Process(label, startTime, burstTime)
+      process.runTime = runTime
+      process.endTime = endTime
+      burst = Burst(process)
+      burst.startTime = burstStartTime
+      burst.length = burstLength
+      bursts.append(burst)
+    gantt = Gantt()
+    gantt.time = time
+    gantt.bursts = bursts
+    return gantt
 def parseList(listString):
   listItem = listString.strip(',').strip(' ').strip(',').split(',')
   listItem = list(map(lambda item: int(item.strip(' ')), listItem))
