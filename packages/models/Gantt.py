ca129f82f2f1e72947d186245dc1cafa16ae13c8
Navid <navid.naseri.079@gmail.com>
2021-12-30 18:41:22 +0330

    Gantt model added

---
+++ packages/models/Gantt.py
@@ -0,0 +1,23 @@
+from .Burst import Burst
+
+class Gantt:
+  time = 0
+
+  def __init__(self):
+    self.bursts = []
+  
+  def addBurst(self, process):
+    if self.getLastBurst().process is process:
+      return
+    self.bursts.append(Burst(process))
+
+  def getLastBurst(self):
+    if not self.bursts: return None
+    return self.bursts[-1]
+
+  def stepInto(self):
+    self.getLastBurst().stepInto()
+    self.time += 1
+  
+  def finishLastBurst(self):
+    self.time += self.getLastBurst().runTillEnd()
